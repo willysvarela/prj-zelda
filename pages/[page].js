44a9b881324d5638ab0ef543e130814502866678
willysv <willysv@motorola.com>
2021-08-13 17:31:00 -0400

    feat: Page component

---
+++ pages/[page].js
@@ -0,0 +1,34 @@
+import { useRouter } from "next/router";
+
+const pageData = {
+  id: 1,
+  name: "willysvarela",
+  description: "",
+  links: [
+    { name: "Instagram", url: "https://instagram.com/willysvarela", backgroundColor: "#ccc" },
+    { name: "twitter", url: "https://twitter.com/willysvarela", backgroundColor: "#ccc" }
+  ]
+};
+
+const Page = () => {
+  const router = useRouter();
+  const { page } = router.query;
+
+	const renderLink = (link) => {
+		return (
+			<li key={link.id}><a target="_blank" href={link.url} rel="noreferrer">{link.name}</a></li>
+		)
+	}
+
+  return (
+    <div>
+      Page: {pageData.name}
+      <ul>{pageData.links.map((link) => renderLink(link))}</ul>
+    </div>
+  );
+
+	
+
+};
+
+export default Page;
